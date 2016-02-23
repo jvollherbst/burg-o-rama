57a4b5bbcab59da55b48341c6bbf4b2a59c610d3
jvollherbst <jasmine.cardoza@gmail.com>
2016-02-23 17:31:45 -0500

    fixed naming error in my schema.sql file. changed my order table to burgers

--- burger-project/db/schema.sql
+++ burger-project/db/schema.sql
@@ -1,44 +1,51 @@
-DROP TABLE IF EXISTS burgers_db;
+DROP TABLE IF EXISTS meat;
+DROP TABLE IF EXISTS buns;
+DROP TABLE IF EXISTS cheese;
+DROP TABLE IF EXISTS toppings;
+DROP TABLE IF EXISTS burgers;
+DROP TABLE IF EXISTS cheese_join;
+DROP TABLE IF EXISTS toppings_join;
 
-CREATE TABLE orders (
-  burgers_id SERIAL UNIQUE PRIMARY KEY,
-  doneness text,
-  meat_id FOREIGN KEY REFERENCES meat(meat_id),
-  buns_id FOREIGN KEY REFERENCES buns(buns_id)
-  -- cheese_id FOREIGN KEY REFERENCES cheese(cheese_id),
-  -- toppings_id FOREIGN KEY REFERENCES toppings(toppings_id),
-)
 
 CREATE TABLE meat (
-  meat_id SERIAL UNIQUE PRIMARY KEY,
+  meat_id SERIAL PRIMARY KEY,
   type text,
   available boolean
-)
+);
 
 CREATE TABLE buns (
-  buns_id SERIAL UNIQUE PRIMARY KEY,
+  buns_id SERIAL PRIMARY KEY,
   type text,
   available boolean
-)
+);
 
 CREATE TABLE cheese (
-  cheese_id SERIAL UNIQUE PRIMARY KEY,
+  cheese_id SERIAL PRIMARY KEY,
   name text,
   available boolean
-)
+);
 
 CREATE TABLE toppings (
-  toppings_id SERIAL UNIQUE PRIMARY KEY,
+  toppings_id SERIAL PRIMARY KEY,
   name text,
   available boolean
-)
+);
 
-CREATE TABLE cheese_join(
-  burgers_id FOREIGN KEY REFERENCES burgers, -- PK, FK1 burgers_id Primary key for this table is a combo of PK, FK1
-  cheese_id FOREIGN KEY REFERENCES cheese -- PK, FK2 cheese_id
-)
---create index on name of table col
-CREATE TABLE toppings_join(
-  burgers_id FOREIGN KEY REFERENCES burgers,
-  toppings_id FOREIGN KEY REFERENCES toppings
-)
+CREATE TABLE burgers (
+  burgers_id serial PRIMARY KEY,
+  doneness text,
+  meat_id integer REFERENCES meat,
+  buns_id integer REFERENCES buns
+);
+
+CREATE TABLE cheese_join (
+  burgers_id integer REFERENCES burgers,
+  cheese_id integer REFERENCES cheese,
+  PRIMARY KEY (burgers_id, cheese_id)
+);
+
+CREATE TABLE toppings_join (
+  burgers_id integer REFERENCES burgers,
+  toppings_id integer REFERENCES toppings,
+  PRIMARY KEY (burgers_id, toppings_id)
+);
